6946fdc6077ca3e0dba329f1c61bb9b62082d012
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-01-05 14:30:18 +0300

    Add files via upload

--- notebook/views.py
+++ notebook/views.py
@@ -3,17 +3,19 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from .models import User
 from .forms import UserForm
+#show all USERS 
 def index(request):
 	users = User.objects.all()
 	return render(request,'main/notebook.html',{'title':'Page of users','users':users})
 
+#to add user with help of method POST
 def adduser(request):
 	error=""
 	if request.method =="POST":
 		form = UserForm(request.POST)
 		if form.is_valid():
 			form.save()
-			redirect('')
+			
 		else:
 			error="Form is uncorrect"
 		  	
@@ -29,4 +31,4 @@ def get_user(request,pk):
 	user= User.objects.get(pk=pk)
 	#user = User.objects.filter(user_id)
 	return render(request,'main/user.html',{'user':user})
-# Create your views here.
+
