0e72050611576ff1e733c89a07d2830d8e4c332f
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-01-09 10:01:58 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('about/', views.adduser),
+    path('user/<int:pk>/',views.get_user),
+    path('search/',views.search),
+    path('search_user/',views.search_user_in_data_base),
+    
     
 ]
